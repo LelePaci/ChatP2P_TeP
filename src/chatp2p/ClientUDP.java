0d1f49538748e0ed56a865fe269ea504d8f2dac2
Emanuele <paciemanuele.2@gmail.com>
2021-11-16 23:23:48 +0100

    Creato progetto Java

---
+++ src/chatp2p/ClientUDP.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatp2p;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pacie
+ */
+public class ClientUDP extends Thread {
+
+    private int port = 2003;
+    private DatagramSocket client;
+    
+    private InetAddress lastAddress;    
+
+    public ClientUDP() throws SocketException {
+        this.port = 2003;
+        this.client = new DatagramSocket(port);
+        this.lastAddress = null;
+
+        
+
+        
+    }
+
+    @Override
+
+    public void run() {
+        while(true){
+            try {
+                ricevi();
+            } catch (IOException ex) {
+                Logger.getLogger(ClientUDP.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public Messaggio ricevi() throws IOException{
+        byte[] buffer = new byte[1500];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        client.receive(packet);
+        String messaggioRicevuto = new String(packet.getData(), 0, packet.getLength());
+        lastAddress = packet.getAddress();
+        
+        System.out.println(messaggioRicevuto);
+        return Messaggio.fromCSV(messaggioRicevuto);
+    }
+    
+    public void invia(String risposta) throws IOException{
+        byte[] responseBuffer = risposta.getBytes();
+        DatagramPacket responsePacket = new DatagramPacket(responseBuffer, responseBuffer.length);
+        responsePacket.setAddress(lastAddress);
+        responsePacket.setPort(port);
+        client.send(responsePacket);
+    }
+}
---
+++ src/chatp2p/Messaggio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatp2p;
+
+/**
+ *
+ * @author pacie
+ */
+public class Messaggio {
+
+    String comando;
+    String dati;
+
+    public Messaggio(String comando, String dati) {
+        this.comando = comando;
+        this.dati = dati;
+    }
+
+    public static Messaggio fromCSV(String csv) {
+        int index = csv.indexOf(";");
+        String c = csv.substring(0, index);
+        String d = csv.substring(index + 1, csv.length());
+        return new Messaggio(c, d);
+    }
+
+}
